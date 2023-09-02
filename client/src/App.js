717346e466dc91721e70801e2f19276e512f4c22
Shawn McCann <smccann0011@gmail.com>
2023-09-02 15:22:48 -0700

    Fix api call

--- client/src/App.js
+++ client/src/App.js
@@ -40,7 +40,7 @@ const JarvisClient = () => {
 
     try {
       console.log("Sending message to server:", userMessage)
-      const response = await axios.post("/api/agent1", {
+      const response = await axios.post("http://127.0.0.1:5000/api/chat", {
         message: userMessage,
       });
       console.log("Received response from server:", response)
